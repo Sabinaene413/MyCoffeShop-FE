41f8c8a46596db659e221ab57fa9d5d52a3136f9
Sabinaene413 <e.ene.sabina.s@gmail.com>
2024-05-03 23:04:25 +0300

    hf

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
     "./node_modules/flowbite-datepicker/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        latte: {
+          100: '#8a553e',
+          200: '#d0a074',
+          300: '#e2cebd',
+          400: '#3c2621',
+          500: '#eae6e6'
+        }
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')({
