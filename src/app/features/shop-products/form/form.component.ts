7ae6b354b5f91be7aad4a49237471bffd9366e09
Sabinaene413 <e.ene.sabina.s@gmail.com>
2024-05-16 20:19:27 +0300

    added product

--- src/app/features/shop-products/form/form.component.ts
+++ src/app/features/shop-products/form/form.component.ts
@@ -6,41 +6,44 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ApiShopProductService } from '../api.service.shop-products';
 import { ShopProduct } from 'src/app/features/shop-products/shop-product-models';
+import { BaseFormComponent } from 'src/app/shared/base-form';
 
 @Component({
   selector: 'app-shopProduct',
   templateUrl: './form.component.html',
 })
-export class ShopProductFormComponent implements OnInit {
-  shopProductForm!: FormGroup;
+export class ShopProductFormComponent
+  extends BaseFormComponent<ShopProduct>
+  implements OnInit
+{
   responseMsg: string = '';
 
   constructor(
-    private formBuilder: FormBuilder,
-    private api: ApiShopProductService,
-    private router: Router
+    public override api: ApiShopProductService,
+    public override router: Router,
+    public override route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) {
-    this.shopProductForm = this.formBuilder.group({
-      name: ['', [Validators.required]],
-      price: ['', [Validators.required]],
-      description: [''],
+    super(api, router, route);
+  }
+
+  override ngOnInit(): void {
+    super.ngOnInit();
+  }
+
+  initializeFormGroup(data: ShopProduct | undefined = undefined) {
+    this.formGroup = this.formBuilder.group({
+      id: [data?.id, [Validators.required]],
+      name: [data?.name ?? '', [Validators.required]],
+      price: [data?.price ?? '', [Validators.required]],
+      description: [data?.description ?? ''],
     });
   }
 
-  ngOnInit(): void {}
-
-  addProduct() {
-    let productData: ShopProduct = {
-      name: this.shopProductForm.get('name')?.value,
-      price: this.shopProductForm.get('price')?.value,
-      description: this.shopProductForm.get('description')?.value,
-    };
-
-    this.api.add(productData).subscribe((response) => {
-      console.log('Product added successfully:', response);
-    });
+  override afterSave() {
+    this.router.navigate(['/shop-products']);
   }
 }
--- src/app/shared/base-form.ts
+++ src/app/shared/base-form.ts
@@ -1,33 +1,66 @@
-import { Component, Injectable, OnInit } from "@angular/core";
-import { Router, ActivatedRoute } from "@angular/router";
-import { AbstractApiService } from "./base-api.service";
-
+import { Component, Injectable, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AbstractApiService } from './base-api.service';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root',
 })
 export abstract class BaseFormComponent<DTO> implements OnInit {
-    public isEditMode: boolean | undefined;
-    public entityId: number | undefined;
-    constructor(
-      public api: AbstractApiService<DTO>,
-      public router: Router,
-      public route: ActivatedRoute
-    ) {}
-  
-    ngOnInit(): void {
-      this.route.paramMap.subscribe((params) => {
-        const id = params.get('id');
-        if (id) {
-          this.entityId = +id;
-          this.isEditMode = true;
-          // Fetch data and populate form for editing
-          this.api.getById(this.entityId).subscribe((data) => {
-            this.initializeFormGroup(data);
-          });
-        } else this.initializeFormGroup(undefined);
+  public data!: any;
+  public saveData!: any;
+  public formGroup!: FormGroup;
+  public isEditMode: boolean | undefined;
+  public entityId: number | undefined;
+  constructor(
+    public api: AbstractApiService<DTO>,
+    public router: Router,
+    public route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      if (id) {
+        this.entityId = +id;
+        this.isEditMode = true;
+        // Fetch data and populate form for editing
+        this.api.getById(this.entityId).subscribe((data) => {
+          this.data = data;
+          this.initializeFormGroup(this.data);
+        });
+      } else this.initializeFormGroup(undefined);
+    });
+  }
+
+  mapToSaveData<DTO>() {
+    this.saveData = this.getFormData<DTO>(this.formGroup);
+  }
+
+  getFormData<DTO>(form: FormGroup): DTO {
+    const formData: any = {};
+    Object.keys(form.controls).forEach((key) => {
+      formData[key] = form.get(key)?.value;
+    });
+    return formData as DTO;
+  }
+
+  public save() {
+    this.mapToSaveData();
+    if (this.entityId) {
+      this.api.update(this.saveData).subscribe((response) => {
+        this.afterSave();
+        console.log('Entity added successfully:', response);
+      });
+    } else {
+      this.api.add(this.saveData).subscribe((response) => {
+        this.afterSave();
+        console.log('Entity added successfully:', response);
       });
     }
-  
-    public abstract initializeFormGroup(data: DTO | undefined): void;
   }
+
+  afterSave() {}
+
+  abstract initializeFormGroup(data: DTO | undefined): void;
+}
